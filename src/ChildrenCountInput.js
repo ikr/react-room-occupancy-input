e011a33f35492c2c13cdfe30d188e97587e5b0ce
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-10 09:24:51 +0100

    Extract integerValueSelectClass

--- src/ChildrenCountInput.js
+++ src/ChildrenCountInput.js
@@ -1,26 +1,5 @@
 (function () {
     'use strict';
-
-    var React = require('react');
-
-    module.exports = React.createClass({
-        propTypes: {
-            value: React.PropTypes.number,
-            onChange: React.PropTypes.func
-        },
-
-        render: function () {
-            return React.DOM.select(
-                {value: this.props.value, onChange: this.handleSelectionChange},
-
-                [0, 1, 2, 3, 4, 5].map(function (v) {
-                    return React.DOM.option({value: v, key: 'k' + v}, v);
-                })
-            );
-        },
-
-        handleSelectionChange: function (event) {
-            this.props.onChange(parseInt(event.target.value, 10));
-        }
-    });
+    var integerValueSelectClass = require('./integerValueSelectClass');
+    module.exports = integerValueSelectClass([0, 1, 2, 3, 4, 5]);
 }());
---
+++ src/integerValueSelectClass.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    var React = require('react');
+
+    module.exports = function (options) {
+        return React.createClass({
+            propTypes: {
+                value: React.PropTypes.number,
+                onChange: React.PropTypes.func
+            },
+
+            render: function () {
+                return React.DOM.select(
+                    {value: this.props.value, onChange: this.handleSelectionChange},
+
+                    options.map(function (v) {
+                        return React.DOM.option({value: v, key: 'k' + v}, v);
+                    })
+                );
+            },
+
+            handleSelectionChange: function (event) {
+                this.props.onChange(parseInt(event.target.value, 10));
+            }
+        });
+    };
+}());
