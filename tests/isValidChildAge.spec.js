494bb5019755306b93085b87f505c7b9c13e2f51
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-10 15:44:53 +0100

    Confirm that an empty child age is invalid

--- tests/isValidChildAge.spec.js
+++ tests/isValidChildAge.spec.js
@@ -12,12 +12,13 @@ describe('isValidChildAge', function () {
         ['12', true],
         ['13', false],
         [2.5, false],
-        ['Infinity', false]
+        ['Infinity', false],
+        ['', false]
     ].forEach(function (testCase) {
         var value = testCase[0],
             expectedResult = testCase[1];
 
-        it('is ' + expectedResult + ' for ' + value, function () {
+        it('is ' + expectedResult + ' for "' + value + '"', function () {
             assert.strictEqual(isValidChildAge(value), expectedResult);
         });
     });
