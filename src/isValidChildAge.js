df20c0f1f99cfd649c4b5e0d02a14defe0bb5b51
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-10 12:38:33 +0100

    Add isValidChildAge

---
+++ src/isValidChildAge.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    if (!Number.isInteger) {
+        Number.isInteger = function isInteger (nVal) {
+            return (
+                (typeof nVal === 'number') &&
+                isFinite(nVal) &&
+                (nVal > -9007199254740992) &&
+                (nVal < 9007199254740992) &&
+                (Math.floor(nVal) === nVal)
+            );
+        };
+    }
+
+    module.exports = function (x) {
+        var floatX = parseFloat(x);
+        return Number.isInteger(floatX) && (floatX >= 0) && (floatX <= 12);
+    };
+}());
---
+++ tests/isValidChildAge.spec.js
@@ -0,0 +1,24 @@
+describe('isValidChildAge', function () {
+    'use strict';
+
+    var assert = require('assert'),
+        isValidChildAge = require('../src/isValidChildAge');
+
+    [
+        ['0', true],
+        [7, true],
+        ['-1', false],
+        ['bla', false],
+        ['12', true],
+        ['13', false],
+        [2.5, false],
+        ['Infinity', false]
+    ].forEach(function (testCase) {
+        var value = testCase[0],
+            expectedResult = testCase[1];
+
+        it('is ' + expectedResult + ' for ' + value, function () {
+            assert.strictEqual(isValidChildAge(value), expectedResult);
+        });
+    });
+});
