4875fdb5e2e750a402bddf2047ca27651b0791e5
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-11 10:16:42 +0100

    Render the ChildrenInput sub-elements

--- src/ChildrenInput.js
+++ src/ChildrenInput.js
@@ -1,7 +1,9 @@
 (function () {
     'use strict';
 
-    var React = require('react');
+    var React = require('react'),
+        ChildrenCountInput = require('./ChildrenCountInput'),
+        ChildAgeInput = require('./ChildAgeInput');
 
     module.exports = React.createClass({
         propTypes: {
@@ -9,7 +11,31 @@
         },
 
         render: function () {
-            return React.DOM.div();
+            return React.DOM.div({className: 'room-occupancy-children'}, this.subElements());
+        },
+
+        subElements: function () {
+            return [this.countElement()].concat(this.ageElements());
+        },
+
+        countElement: function () {
+            return React.createElement(ChildrenCountInput, {
+                ref: 'count',
+                value: this.props.value.length,
+                key: 'count'
+            });
+        },
+
+        ageElements: function () {
+            return this.props.value.map(function (child, index) {
+                var id = 'age' + index;
+
+                return React.createElement(ChildAgeInput, {
+                    ref: id,
+                    value: child.age,
+                    key: id
+                });
+            });
         }
     });
 }());
--- tests/ChildrenInput.spec.js
+++ tests/ChildrenInput.spec.js
@@ -4,6 +4,7 @@ describe('ChildrenInput', function () {
     var assert = require('assert'),
         ChildrenInput = require('../src/ChildrenInput'),
         ChildrenCountInput = require('../src/ChildrenCountInput'),
+        ChildAgeInput = require('../src/ChildAgeInput'),
         React = require('react'),
         TestUtils = require('react/addons').addons.TestUtils,
         TestBrowser = require('jsdom-test-browser'),
@@ -17,17 +18,59 @@ describe('ChildrenInput', function () {
         beforeEach(function (done) { bro.setUp(done); });
         afterEach(function () { bro.tearDown(); });
 
-        describe('rendering according to its value', function () {
+        describe('HTML', function () {
             var element;
 
             beforeEach(function () {
                 element = TestUtils.renderIntoDocument(
-                    React.createElement(ChildrenInput, {value: [{age: 0}, {age: 2}, {age: 12}]})
+                    React.createElement(ChildrenInput, {value: []})
                 ).getDOMNode();
             });
 
-            it('includes a ChildrenCountInput', function () {
-                assert(TestUtils.isElementOfType(bro.$('select').get(0), ChildrenCountInput));
+            it('has the top level class assigned', function () {
+                assert(bro.$(element).hasClass('room-occupancy-children'));
+            });
+        });
+
+        describe('rendering according to its value', function () {
+            var component,
+                element;
+
+            beforeEach(function () {
+                component = TestUtils.renderIntoDocument(
+                    React.createElement(ChildrenInput, {value: [{age: 0}, {age: 2}, {age: 12}]})
+                );
+
+                element = component.getDOMNode();
+            });
+
+            it('includes a referenced ChildrenCountInput', function () {
+                assert(
+                    TestUtils.isCompositeComponentWithType(component.refs.count, ChildrenCountInput)
+                );
+            });
+
+            it('sets the count value', function () {
+                assert.strictEqual(component.refs.count.props.value, 3);
+            });
+
+            it('displays the correct amount of age inputs', function () {
+                assert.strictEqual(bro.$('input[type=number]', element).size(), 3);
+            });
+
+            [0, 2, 12].forEach(function (age, index) {
+                it('includes the reference to the ChildAgeInput #' + index, function () {
+                    assert(
+                        TestUtils.isCompositeComponentWithType(
+                            component.refs['age' + index],
+                            ChildAgeInput
+                        )
+                    );
+                });
+
+                it('sets the age #' + index + ' value', function () {
+                    assert.strictEqual(component.refs['age' + index].props.value, age);
+                });
             });
         });
     });
