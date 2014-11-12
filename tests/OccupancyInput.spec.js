ffbf7f5d28c3b7315b73ae18f420f3ed3ffb3fde
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-12 14:58:36 +0100

    Flatten the tests a bit

--- tests/OccupancyInput.spec.js
+++ tests/OccupancyInput.spec.js
@@ -18,68 +18,66 @@ describe('OccupancyInput', function () {
     });
 
     describe('instance', function () {
+        var component,
+            element;
+
         before(function (done) { bro.jQueryify(done); });
 
-        describe('structure', function () {
-            var component,
-                element;
+        beforeEach(function () {
+            component = TestUtils.renderIntoDocument(
+                React.createElement(OccupancyInput, {
+                    value: {adults: 1, children: []},
+                    onChange: function () {}
+                })
+            );
 
-            beforeEach(function () {
-                component = TestUtils.renderIntoDocument(
-                    React.createElement(OccupancyInput, {
-                        value: {adults: 1, children: []},
-                        onChange: function () {}
-                    })
-                );
+            element = component.getDOMNode();
+        });
 
-                element = component.getDOMNode();
-            });
+        it('has DIV as a top level tag', function () {
+            assert.strictEqual(element.tagName, 'DIV');
+        });
 
-            it('has DIV as a top level tag', function () {
-                assert.strictEqual(element.tagName, 'DIV');
-            });
+        it('has the top level class assigned', function () {
+            assert(bro.$(element).hasClass('room-occupancy'));
+        });
 
-            it('has the top level class assigned', function () {
-                assert(bro.$(element).hasClass('room-occupancy'));
-            });
+        it('aggregates an AdultsCountInput', function () {
+            assert(
+                TestUtils.isCompositeComponentWithType(component.refs.adults, AdultsCountInput)
+            );
+        });
 
-            it('aggregates an AdultsCountInput', function () {
-                assert(
-                    TestUtils.isCompositeComponentWithType(component.refs.adults, AdultsCountInput)
-                );
-            });
+        it('passes down the adults count', function () {
+            assert.strictEqual(component.refs.adults.props.value, 1);
+        });
 
-            it('passes down the adults count', function () {
-                assert.strictEqual(component.refs.adults.props.value, 1);
-            });
+        it('provides the adults count change handler', function () {
+            assert(component.handleAdultsChange);
 
-            it('provides the adults count change handler', function () {
-                assert(component.handleAdultsChange);
+            assert.strictEqual(
+                component.refs.adults.props.onChange,
+                component.handleAdultsChange
+            );
+        });
 
-                assert.strictEqual(
-                    component.refs.adults.props.onChange,
-                    component.handleAdultsChange
-                );
-            });
+        it('aggregates a ChildrenInput', function () {
+            assert(
+                TestUtils.isCompositeComponentWithType(component.refs.children, ChildrenInput)
+            );
+        });
 
-            it('aggregates a ChildrenInput', function () {
-                assert(
-                    TestUtils.isCompositeComponentWithType(component.refs.children, ChildrenInput)
-                );
-            });
+        it('passes down the children array', function () {
+            assert.deepEqual(component.refs.children.props.value, []);
+        });
 
-            it('passes down the children array', function () {
-                assert.deepEqual(component.refs.children.props.value, []);
-            });
+        it('provides the children change handler', function () {
+            assert(component.handleChildrenChange);
 
-            it('provides the children change handler', function () {
-                assert(component.handleChildrenChange);
-
-                assert.strictEqual(
-                    component.refs.children.props.onChange,
-                    component.handleChildrenChange
-                );
-            });
+            assert.strictEqual(
+                component.refs.children.props.onChange,
+                component.handleChildrenChange
+            );
         });
     });
 });
