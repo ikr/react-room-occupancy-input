6078521282351c02330912ea2e32b59fffa36582
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-12 14:53:33 +0100

    Define OccupancyInput elements' structure

--- src/OccupancyInput.js
+++ src/OccupancyInput.js
@@ -1,7 +1,9 @@
 (function () {
     'use strict';
 
-    var React = require('react');
+    var React = require('react'),
+        AdultsCountInput = require('./AdultsCountInput'),
+        ChildrenInput = require('./ChildrenInput');
 
     module.exports = React.createClass({
         propTypes: {
@@ -10,7 +12,24 @@
         },
 
         render: function () {
-            return React.DOM.div({className: 'room-occupancy'}, '');
-        }
+            return React.DOM.div({className: 'room-occupancy'}, [
+                React.createElement(AdultsCountInput, {
+                    ref: 'adults',
+                    value: this.props.value.adults,
+                    onChange: this.handleAdultsChange,
+                    key: 'k0'
+                }),
+
+                React.createElement(ChildrenInput, {
+                    ref: 'children',
+                    value: this.props.value.children,
+                    onChange: this.handleChildrenChange,
+                    key: 'k1'
+                })
+            ]);
+        },
+
+        handleAdultsChange: function () {},
+        handleChildrenChange: function () {}
     });
 }());
--- tests/OccupancyInput.spec.js
+++ tests/OccupancyInput.spec.js
@@ -5,7 +5,9 @@ describe('OccupancyInput', function () {
         bro = require('jsdom-test-browser'),
         React = require('react'),
         TestUtils = require('react/addons').addons.TestUtils,
-        OccupancyInput = require('../src/OccupancyInput');
+        OccupancyInput = require('../src/OccupancyInput'),
+        AdultsCountInput = require('../src/AdultsCountInput'),
+        ChildrenInput = require('../src/ChildrenInput');
 
     it('declares the value property', function () {
         assert(OccupancyInput.propTypes.value);
@@ -18,16 +20,19 @@ describe('OccupancyInput', function () {
     describe('instance', function () {
         before(function (done) { bro.jQueryify(done); });
 
-        describe('HTML', function () {
-            var element;
+        describe('structure', function () {
+            var component,
+                element;
 
             beforeEach(function () {
-                element = TestUtils.renderIntoDocument(
+                component = TestUtils.renderIntoDocument(
                     React.createElement(OccupancyInput, {
                         value: {adults: 1, children: []},
                         onChange: function () {}
                     })
-                ).getDOMNode();
+                );
+
+                element = component.getDOMNode();
             });
 
             it('has DIV as a top level tag', function () {
@@ -36,6 +41,44 @@ describe('OccupancyInput', function () {
 
             it('has the top level class assigned', function () {
                 assert(bro.$(element).hasClass('room-occupancy'));
+            });
+
+            it('aggregates an AdultsCountInput', function () {
+                assert(
+                    TestUtils.isCompositeComponentWithType(component.refs.adults, AdultsCountInput)
+                );
+            });
+
+            it('passes down the adults count', function () {
+                assert.strictEqual(component.refs.adults.props.value, 1);
+            });
+
+            it('provides the adults count change handler', function () {
+                assert(component.handleAdultsChange);
+
+                assert.strictEqual(
+                    component.refs.adults.props.onChange,
+                    component.handleAdultsChange
+                );
+            });
+
+            it('aggregates a ChildrenInput', function () {
+                assert(
+                    TestUtils.isCompositeComponentWithType(component.refs.children, ChildrenInput)
+                );
+            });
+
+            it('passes down the children array', function () {
+                assert.deepEqual(component.refs.children.props.value, []);
+            });
+
+            it('provides the children change handler', function () {
+                assert(component.handleChildrenChange);
+
+                assert.strictEqual(
+                    component.refs.children.props.onChange,
+                    component.handleChildrenChange
+                );
             });
         });
     });
