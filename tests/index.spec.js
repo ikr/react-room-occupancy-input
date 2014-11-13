6dc550642cc78cbe76c4bf6169b488e7f7a206bb
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-13 10:02:03 +0100

    Define the public API

---
+++ tests/index.spec.js
@@ -0,0 +1,14 @@
+describe('Public API', function () {
+    'use strict';
+
+    var assert = require('assert'),
+        bro = require('jsdom-test-browser'),
+        api = require('../index'),
+        OccupancyInput = require('../src/OccupancyInput');
+
+    assert(bro);
+
+    it('is simply the OccupancyInput class', function () {
+        assert.strictEqual(api, OccupancyInput);
+    });
+});
